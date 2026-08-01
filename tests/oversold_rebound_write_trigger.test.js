3db54e3d02dfb9c0f7058a7fbacbfae31f6f9ee3
Eason Liu <58838721+EasonLiu0913@users.noreply.github.com>
2026-08-01 17:05:34 +0800

    ci: trigger canonical oversold rebound research output

---
+++ tests/oversold_rebound_write_trigger.test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const test = require('node:test');
+const assert = require('node:assert/strict');
+
+test('one-time canonical research output trigger', () => {
+  assert.equal(true, true);
+});
